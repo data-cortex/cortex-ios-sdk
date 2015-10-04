6accbff98e7bf0bf6f82cd09555ef9e2d5b3a5e7
Yanko Bolanos <y@rem7.com>
2015-10-04 13:52:59 -0700

    updated xcode files

--- Example/Pods/Target Support Files/cortex-ios-sdk/cortex-ios-sdk-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_cortex_ios_sdk : NSObject
-@end
-@implementation PodsDummy_cortex_ios_sdk
-@end
--- Example/Pods/Target Support Files/cortex-ios-sdk/cortex-ios-sdk-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "DataCortex.h"
-
-FOUNDATION_EXPORT double cortex_ios_sdkVersionNumber;
-FOUNDATION_EXPORT const unsigned char cortex_ios_sdkVersionString[];
-
